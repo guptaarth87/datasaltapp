6772c56ee61cabf972bfac30753283da60ca60f0
guptaarth87 <guptaarth87@gmail.com>
2023-06-13 14:17:43 +0530

    initial commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Contactus from '../components/Contact/contactus'
+import Contactus from '../components/Contact/Contactus'
 
 export default function Contact() {
   return (
